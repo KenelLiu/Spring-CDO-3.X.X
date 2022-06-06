a703415fbefbf3278aa344c75663137615d31dc9
KenelLiu <KenelLiu@china-liantong.com>
2022-06-06 20:35:51 +0800

    add File

--- CDOUtil/src/main/java/com/cdo/util/bean/CDOHTTPResponse.java
+++ CDOUtil/src/main/java/com/cdo/util/bean/CDOHTTPResponse.java
@@ -12,20 +12,21 @@ public class CDOHTTPResponse extends Response {
 
 	public CDOHTTPResponse() {
 		
-	}
-	
+	}	
 	private static final long serialVersionUID = -6353099618301387046L;
-	private Map<String, File> mapFileMap=new HashMap<String, File>();
+	private Map<String, File> mapFile=new HashMap<String, File>();
 
 	public void addFile(String strField,File fileValue){
-		mapFileMap.put(strField, fileValue);
+		mapFile.put(strField, fileValue);
 	}
 	public File getFile(String strField){
-		return mapFileMap.get(strField);
+		return mapFile.get(strField);
 	}
-	
+	public Map<String, File> mapFile(){
+		return mapFile;
+	}
 	public void copyFile2CDO(CDO cdo){
-		for(Iterator<Map.Entry<String, File>> it=mapFileMap.entrySet().iterator();it.hasNext();){
+		for(Iterator<Map.Entry<String, File>> it=mapFile.entrySet().iterator();it.hasNext();){
 			Entry<String, File> entry=it.next();
 			cdo.setFileValue(entry.getKey(),entry.getValue());
 		}
